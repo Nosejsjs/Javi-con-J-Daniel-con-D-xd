031a95b17651fbfb91931651f1c6adb2bb1dff7d
Daniel del Cid <dancidgar@gmail.com>
2023-04-29 19:36:30 -0600

    termine jij

---
+++ matrices tablas/matrices tablas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace matricesdetablasdemultiplicar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese el tamaño de la matriz: ");
+            int messi = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[messi, messi];
+
+            for (int i = 0; i < messi; i++)
+            {
+                for (int j = 0; j < messi; j++)
+                {
+                    matriz[i, j] = (i + 1) * (j + 1);
+                }
+            }
+
+            Console.WriteLine("Matriz:");
+            for (int i = 0; i < messi; i++)
+            {
+                for (int j = 0; j < messi; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
