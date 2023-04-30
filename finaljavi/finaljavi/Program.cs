8736d33bb4eb961eec2f696aa4debed76aef11d0
Daniel del Cid <dancidgar@gmail.com>
2023-04-29 19:03:36 -0600

    el ejercicio 2

---
+++ finaljavi/finaljavi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ultimacosadejavi
+{
+    class persona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private int altura;
+
+        public persona(string nombre, string apellido, int edad, int altura)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+            this.altura = altura;
+        }
+
+        public string todoelnombre()
+        {
+            return nombre + " " + apellido;
+        }
+        public double alturaenm()
+        {
+            return altura / 100.00;
+        }
+        public void imprimirdata()
+        {
+            Console.WriteLine("Nombre completo: " + todoelnombre());
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Altura: " + alturaenm() + "m");
+        }
+
+    }
+    class persona1
+    {
+        static void Main()
+        {
+            persona persona = new persona("Daniel","del Cid", 18, 181 );
+            persona.imprimirdata();
+        }
+    }
+}
